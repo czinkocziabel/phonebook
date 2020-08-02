8bfec440ca4f634f72e63c1caf99034ec8b0bc1f
czinkocziabel <czinkocziabel@gmail.com>
2020-08-02 19:35:23 +0200

    PhoneBook

--- PhoneBook/src/phonebook/ViewController.java
+++ PhoneBook/src/phonebook/ViewController.java
@@ -16,7 +16,7 @@ import javafx.scene.control.Label;
  *
  * @author Ragnar
  */
-public class FXMLDocumentController implements Initializable {
+public class ViewController implements Initializable {
     
     @FXML
     private Label label;

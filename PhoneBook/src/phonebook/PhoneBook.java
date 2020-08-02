c011ccccc5d5535be1fcf5c18f8ab0337db4dd05
czinkocziabel <czinkocziabel@gmail.com>
2020-08-02 20:05:52 +0200

    PhoneBook

--- PhoneBook/src/phonebook/PhoneBook.java
+++ PhoneBook/src/phonebook/PhoneBook.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package phonebook;
 
 import javafx.application.Application;
@@ -11,27 +6,21 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- *
- * @author Ragnar
- */
 public class PhoneBook extends Application {
-    
+
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
+        Parent root = FXMLLoader.load(getClass().getResource("View.fxml"));
+
         Scene scene = new Scene(root);
-        
+
+        stage.setTitle("Telefonkönyv");
         stage.setScene(scene);
         stage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }

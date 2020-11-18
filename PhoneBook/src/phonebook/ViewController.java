f3655db3f687fc98d7cf6bb149d62f98f2483c35
czinkocziabel <czinkocziabel@gmail.com>
2020-11-18 22:18:40 +0100

    add POJO

--- PhoneBook/src/phonebook/ViewController.java
+++ PhoneBook/src/phonebook/ViewController.java
@@ -2,9 +2,32 @@ package phonebook;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 
 public class ViewController implements Initializable {
+
+    @FXML
+    TableView table;
+    @FXML
+    TextField inputLastName;
+    @FXML
+    TextField inputFirstName;
+    @FXML
+    TextField inputEmail;
+    @FXML
+    Button addNewContactButton;
+    @FXML
+    StackPane menuPane;
+    @FXML
+    Pane contactPane;
+    @FXML
+    Pane exportPane;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {

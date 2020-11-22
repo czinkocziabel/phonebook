8b5e9fc8f36aa4c4848d186b5e1d20381c3619b7
czinkocziabel <czinkocziabel@gmail.com>
2020-11-22 20:58:38 +0100

    update

--- PhoneBook/src/phonebook/ViewController.java
+++ PhoneBook/src/phonebook/ViewController.java
@@ -1,18 +1,26 @@
 package phonebook;
 
 import java.net.URL;
-import java.util.Observable;
 import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 
@@ -35,31 +43,113 @@ public class ViewController implements Initializable {
     @FXML
     Pane exportPane;
 
+    private final String MENU_CONTACTS = "Kontaktok";
+    private final String MENU_LIST = "Lista";
+    private final String MENU_EXPORT = "Exportálás";
+    private final String MENU_EXIT = "Kilépés";
+
     private final ObservableList<Person> data = FXCollections.observableArrayList(
             new Person("Gyula", "Szabó", "gyula@gmail.com"),
             new Person("Jason", "Bourne", "jason@citromail.hu"),
             new Person("Michael", "Scott", "michael@freemail.com")
     );
 
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
+    private void setTableData() {
         TableColumn lastNameCol = new TableColumn("Vezetéknév");
         lastNameCol.setMinWidth(100);
         lastNameCol.setCellFactory(TextFieldTableCell.forTableColumn());
-        lastNameCol.setCellValueFactory(new PropertyValueFactory<Person,String>("lastName"));
-        
-        TableColumn fistNameCol = new TableColumn("Keresztnév");
-        fistNameCol.setMinWidth(100);
-        fistNameCol.setCellFactory(TextFieldTableCell.forTableColumn());
-        fistNameCol.setCellValueFactory(new PropertyValueFactory<Person,String>("firstName"));
-        
+        lastNameCol.setCellValueFactory(new PropertyValueFactory<Person, String>("lastName"));
+
+        lastNameCol.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<Person, String>>() {
+            @Override
+            public void handle(TableColumn.CellEditEvent<Person, String> event) {
+                ((Person) event.getTableView().getItems().get(event.getTablePosition().getRow()))
+                        .setLastName(event.getNewValue());
+            }
+        });
+
+        TableColumn firstNameCol = new TableColumn("Keresztnév");
+        firstNameCol.setMinWidth(100);
+        firstNameCol.setCellFactory(TextFieldTableCell.forTableColumn());
+        firstNameCol.setCellValueFactory(new PropertyValueFactory<Person, String>("firstName"));
+
+        firstNameCol.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<Person, String>>() {
+            @Override
+            public void handle(TableColumn.CellEditEvent<Person, String> event) {
+                ((Person) event.getTableView().getItems().get(event.getTablePosition().getRow()))
+                        .setFirstName(event.getNewValue());
+            }
+        });
+
         TableColumn emailCol = new TableColumn("Email cím");
         emailCol.setMinWidth(200);
         emailCol.setCellFactory(TextFieldTableCell.forTableColumn());
-        emailCol.setCellValueFactory(new PropertyValueFactory<Person,String>("email"));
-        
-        table.getColumns().addAll(lastNameCol,fistNameCol,emailCol);
+        emailCol.setCellValueFactory(new PropertyValueFactory<Person, String>("email"));
+
+        emailCol.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<Person, String>>() {
+            @Override
+            public void handle(TableColumn.CellEditEvent<Person, String> event) {
+                ((Person) event.getTableView().getItems().get(event.getTablePosition().getRow()))
+                        .setEmail(event.getNewValue());
+            }
+        });
+
+        table.getColumns().addAll(lastNameCol, firstNameCol, emailCol);
         table.setItems(data);
+
+    }
+
+    private void setMenuData() {
+        TreeItem<String> treeItemRoot1 = new TreeItem<>("Menü");
+        TreeView<String> treeView = new TreeView<>(treeItemRoot1);
+        treeView.setShowRoot(false);
+
+        TreeItem<String> nodeItemA = new TreeItem<>(MENU_CONTACTS);
+        TreeItem<String> nodeItemB = new TreeItem<>(MENU_EXIT);
+
+        Node contactNode = new ImageView(new Image(getClass().getResourceAsStream("/contact.png")));
+        Node exportNode = new ImageView(new Image(getClass().getResourceAsStream("/export.png")));
+
+        TreeItem<String> nodeItemA1 = new TreeItem<>(MENU_LIST, contactNode);
+        TreeItem<String> nodeItemA2 = new TreeItem<>(MENU_EXPORT, exportNode);
+
+        nodeItemA.getChildren().addAll(nodeItemA1, nodeItemA2);
+        nodeItemA.setExpanded(true);
+        treeItemRoot1.getChildren().addAll(nodeItemA, nodeItemB);
+
+        menuPane.getChildren().add(treeView);
+
+        treeView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener() {
+            @Override
+            public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+                TreeItem<String> selectedItem = (TreeItem<String>) newValue;
+                String selectedMenu = selectedItem.getValue();
+
+                if (null != selectedMenu) {
+                    switch (selectedMenu) {
+                        case MENU_LIST:
+                            contactPane.setVisible(true);
+                            exportPane.setVisible(false);
+                            break;
+                        case MENU_EXPORT:
+                            contactPane.setVisible(false);
+                            exportPane.setVisible(true);
+                            break;
+                        case MENU_EXIT:
+                            System.exit(0);
+                            break;
+                    }
+
+                }
+
+            }
+        });
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        setTableData();
+        setMenuData();
     }
 
 }
